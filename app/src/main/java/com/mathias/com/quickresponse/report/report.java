0b24709591ced3fef37af959ad6e7d72919676e0
AnguleMathias <angulemathias3@gmail.com>
2019-04-25 16:08:59 +0300

    location commit

--- app/src/main/java/com/mathias/com/quickresponse/report/report.java
+++ app/src/main/java/com/mathias/com/quickresponse/report/report.java
@@ -1,7 +1,14 @@
 
 package com.mathias.com.quickresponse.report;
 
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
 import android.os.Bundle;
+import android.provider.Settings;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
@@ -18,8 +25,15 @@ import org.json.JSONObject;
 public class report extends AppCompatActivity {
     EditText loaction, descupriction;
     String select;
+    LocationManager locationManager;
+    double longitudeBest, latitudeBest;
+    double longitudeGPS, latitudeGPS;
+    double longitudeNetwork, latitudeNetwork;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
+
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_report);
         Bundle bundle = getIntent().getExtras();
@@ -41,7 +55,83 @@ public class report extends AppCompatActivity {
 
     }
 
-    public JSONArray makejson(){
+    private boolean isLocationEnabled(){
+        return locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER);
+    }
+
+
+    private void showAlert() {
+        final AlertDialog.Builder dialog = new AlertDialog.Builder(this);
+        dialog.setTitle("Enable Location")
+                .setMessage("Your Locations Settings is set to 'Off'.\nPlease Enable Location to " +
+                        "use this app")
+                .setPositiveButton("Location Settings", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface paramDialogInterface, int paramInt) {
+                        Intent myIntent = new Intent(Settings.ACTION_LOCATION_SOURCE_SETTINGS);
+                        startActivity(myIntent);
+                    }
+                })
+                .setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface paramDialogInterface, int paramInt) {
+                    }
+                });
+        dialog.show();
+    }
+
+
+    public void toggleGPSUpdates(View view) {
+        if(!checkLocation())
+            return;
+        CheckBox checkBox = (CheckBox) view;
+        if(checkBox.isChecked()) {
+            locationManager.requestLocationUpdates(
+                    LocationManager.GPS_PROVIDER, 2 * 60 * 1000, 10, locationListenerGPS);
+        }
+    }
+
+
+
+
+    private boolean checkLocation() {
+        if(!isLocationEnabled())
+            showAlert();
+        return isLocationEnabled();
+    }
+
+
+    private final LocationListener locationListenerGPS = new LocationListener() {
+        public void onLocationChanged(Location location) {
+            longitudeGPS = location.getLongitude();
+            latitudeGPS = location.getLatitude();
+
+            runOnUiThread(new Runnable() {
+                @Override
+                public void run() {
+                    loaction.setText((int)longitudeGPS);
+
+                }
+            });
+        }
+
+        @Override
+        public void onStatusChanged(String provider, int status, Bundle extras) {
+
+        }
+
+        @Override
+        public void onProviderEnabled(String provider) {
+
+        }
+
+        @Override
+        public void onProviderDisabled(String provider) {
+
+        }
+
+
+        public JSONArray makejson(){
         JSONArray array= new JSONArray();
         JSONObject jsonObject = new JSONObject();
         try{
